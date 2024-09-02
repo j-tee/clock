2abda3c84da5906e429836fb9ce5bb508a3b0051
j-tee <juliustetteh@gmail.com>
2024-09-02 07:05:41 +0000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { DisplayState } from './helpers';
+import TimeSetter from './TimeSetter';
+import Display from './Display';
+import AlarmSound from "./assets/AlarmSound.mp3"
 
 function App() {
+  const defaultBreakTime = 5 * 60;
+  const defaultSessionTime = 25 * 60;
+  const min = 60;
+  const max = 60;
+  const interval = 60;
+
+  const [breakTime, setBreakTime] = useState(defaultBreakTime);
+  const [sessionTime, setSessionTime] = useState(defaultSessionTime);
+  const [displayState, setDisplayState] = useState<DisplayState>({
+    time: sessionTime,
+    timeType: "Session",
+    timerRunning: false,
+  });
+  useEffect(() => {
+    let timerID: number;
+    if (!displayState.timerRunning) return;
+
+    if (displayState.timerRunning) {
+      timerID = window.setInterval(decrementDisplay, 1000);
+    }
+
+    return () => {
+      window.clearInterval(timerID);
+    }
+  }, [displayState.timerRunning])
+
+  useEffect(() => {
+    if (displayState.time === 0) {
+      const audio = document.getElementById("beep") as HTMLAudioElement
+      audio.currentTime = 2;
+      audio.play().catch((error) => console.log(error));
+      setDisplayState((prev) => ({
+        ...prev,
+        timeType: prev.timeType === "Session" ? "Break" : "Session",
+        time: prev.timeType === "Session" ? breakTime : sessionTime
+      }))
+    }
+  }, [displayState,breakTime, sessionTime]);
+
+  const reset = () => {
+    setBreakTime(defaultBreakTime);
+    setSessionTime(defaultSessionTime);
+    setDisplayState({
+      time: defaultSessionTime,
+      timeType: "Session",
+      timerRunning: false,
+    });
+
+    const audio = document.getElementById("beep") as HTMLAudioElement;
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  const decrementDisplay = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      time: prev.time - 1
+    }))
+  }
+
+  const startStop = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      timerRunning: !prev.timerRunning
+    }))
+  }
+  const changeBreakTime = (time: number) => {
+    if (displayState.timerRunning) return;
+    setBreakTime(time)
+  }
+
+  const changeSessionTime = (time: number) => {
+    if (displayState.timerRunning) return;
+    setSessionTime(time)
+    setDisplayState({
+      time: time,
+      timeType: "Session",
+      timerRunning: false
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="clock">
+      <div className="setters">
+        <div className="break">
+          <h4 id="break-label">
+            Break Length
+          </h4>
+          <TimeSetter
+            time={breakTime}
+            setTime={changeBreakTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="break"
+          />
+        </div>
+        <div className="session">
+          <h4 id="session-label">
+            Session Length
+          </h4>
+          <TimeSetter
+            time={sessionTime}
+            setTime={changeSessionTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="session"
+          />
+        </div>
+      </div>
+      <Display
+        displayState={displayState}
+        reset={reset}
+        startStop={startStop}
+      />
+      <audio src={AlarmSound} id="beep" />
     </div>
-  );
+  )
 }
 
 export default App;
